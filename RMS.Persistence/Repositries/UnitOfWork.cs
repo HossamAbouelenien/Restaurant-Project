94126c0c2b43384796f137533065802e4ea4a4e4
Mahmoud Ali <mahmoudali6936@gmail.com>
2026-03-31 12:13:58 +0200

    Add generic repository and unit of work patterns
    Introduce IUnitOfWork interface, GenericRepository, and UnitOfWork classes to implement repository and unit of work patterns. Remove unused "Repositries" folder reference from project file. These changes improve data access abstraction and maintainability.

---
+++ RMS.Persistence/Repositries/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using RMS.Domain.Contracts;
+using RMS.Domain.Entities;
+using RMS.Persistence.Data.Contexts;
+
+namespace RMS.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly Dictionary<Type, object> _repositories = [];
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity
+        {
+            var entityType = typeof(TEntity);
+
+            if (_repositories.TryGetValue(entityType, out object? repository))
+            {
+                return (IGenericRepository<TEntity>)repository;
+            }
+            var newRepo = new GenericRepository<TEntity>(_dbContext);
+            _repositories[entityType] = newRepo;
+            return newRepo;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
