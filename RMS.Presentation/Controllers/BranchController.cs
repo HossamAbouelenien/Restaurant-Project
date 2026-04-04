fed7072fb7d0da81a793133a4cbe6d78bbfbcb8c
areejkammoush10 <areejkammoush@gmail.com>
2026-04-04 18:01:51 +0200

    Add BranchController and enhance branch DTO mapping
    - Introduced BranchController with endpoints for branch CRUD operations.
    - Implemented GetBranchByIdAsync in BranchServices.
    - Updated BranchProfile to map address fields from Branch to BranchDTO.
    - Refactored BranchDTO to include address-related properties.
    - Registered BranchServices in DI container.
    - Added folder for branch specifications in project file.

---
+++ RMS.Presentation/Controllers/BranchController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RMS.ServicesAbstraction;
+using RMS.Shared.DTOs.BranchDTOs;
+using RMS.Shared.DTOs.BranchStockDTOs;
+using RMS.Shared.QueryParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BranchController :ControllerBase
+    {
+        private readonly IBranchService _branchService;
+        public BranchController(IBranchService branchService)
+        {
+            _branchService = branchService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BranchStockDTO>>>GetAllBranches()
+        {
+            var BranchStocks = await _branchService.GetAllBranchesAsync();
+            return Ok(BranchStocks);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BranchDTO>> GetBranchStock(int id)
+        {
+            var BranchStock = await _branchService.GetBranchByIdAsync(id);
+            return Ok(BranchStock);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BranchDTO>> UpdateBranchStock(int id, BranchDTO updateBranch)
+        {
+            var BranchStock = await _branchService.UpdateBranchAsync(id, updateBranch);
+            return Ok(BranchStock);
+        }
+    }
+}
--- RMS.Shared/DTOs/BranchDTOs/BranchDTO.cs
+++ RMS.Shared/DTOs/BranchDTOs/BranchDTO.cs
@@ -8,14 +8,18 @@ namespace RMS.Shared.DTOs.BranchDTOs
 {
     public class BranchDTO
     {
-        public class BranchResponseDto
-        {
+        
             public int Id { get; set; }
             public string Name { get; set; } = default!;
-            public string Street { get; set; } = default!;
-            public string City { get; set; } = default!;
             public string Phone { get; set; } = default!;
             public bool IsActive { get; set; } = true;
+
+           //Adress
+            public int BuildingNumber { get; set; }
+            public string Street { get; set; } = default!;
+            public string City { get; set; } = default!;
+            public string? Note { get; set; }
+            public string? SpecialMark { get; set; }
         }
     }
-}
+
