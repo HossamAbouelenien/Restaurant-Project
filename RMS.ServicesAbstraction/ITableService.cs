5ce2274bea93f167f7c7ce609cd276d371c96d3b
areejkammoush10 <areejkammoush@gmail.com>
2026-04-06 00:19:41 +0200

    Add Table management feature with API, service, and DTOs
    Introduces TableController with create endpoint, TableService with validation, AutoMapper profile, specifications for querying, DTOs for table data, and query params. Registers TableService in DI and updates the development DB connection string. Enables backend support for table creation and management.

---
+++ RMS.ServicesAbstraction/ITableService.cs
@@ -0,0 +1,14 @@
+﻿using RMS.Shared.DTOs.TableDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.ServicesAbstraction
+{
+   public interface ITableService
+    {
+        Task<TableDTO> CreateTableAsync(CreateTableDTO dto);
+    }
+}
---
+++ RMS.Shared/QueryParams/TableQueryParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Shared.QueryParams
+{
+   
+        public class TableQueryParams
+        {
+            public int? BranchId { get; set; }
+            public bool? IsOccupied { get; set; }
+        }
+    }
+
