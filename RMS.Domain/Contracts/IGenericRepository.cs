0df75511beb857dbaf695b46ecb8c1929e1c09df
mustafas3aad <mostafasaadtornado7@gmail.com>
2026-04-05 11:47:43 +0200

    Remove commented GetByIdIgnoreFiltersAsync method
    Deleted the commented-out GetByIdIgnoreFiltersAsync method from both the IGenericRepository interface and GenericRepository implementation to clean up unused code. No functional changes were made.

--- RMS.Domain/Contracts/IGenericRepository.cs
+++ RMS.Domain/Contracts/IGenericRepository.cs
@@ -19,6 +19,6 @@ namespace RMS.Domain.Contracts
         Task<TEntity?> GetByIdAsync(ISpecifications<TEntity> specifications);
         Task<int> CountAsync(ISpecifications<TEntity> specifications);
 
-        //Task<TEntity?> GetByIdIgnoreFiltersAsync(int id);
+        
     }
 }
