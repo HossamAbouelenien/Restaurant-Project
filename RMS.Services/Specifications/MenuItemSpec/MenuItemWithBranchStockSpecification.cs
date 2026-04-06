c6fa1c26bbca7f7db8dec8b575b9904c1766cbe4
AmrAlaraby <amr2amr3@gmail.com>
2026-04-06 14:50:03 +0200

    Improve ingredient stock validation in order creation
    Enhanced order creation to check ingredient stock levels across all items before placing an order. Added detailed error messages when stock is insufficient, indicating how many of each menu item can be ordered. Introduced MenuItemWithBranchStockSpecification for eager loading, and extended BranchStockWithBranchAndIngredient for targeted queries. Refactored CreateOrderAsync to use these specifications and new validation logic.

---
+++ RMS.Services/Specifications/MenuItemSpec/MenuItemWithBranchStockSpecification.cs
@@ -0,0 +1,17 @@
+﻿using RMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Services.Specifications.MenuItemSpec
+{
+    internal class MenuItemWithBranchStockSpecification :BaseSpecifications<MenuItem>
+    {
+        public MenuItemWithBranchStockSpecification(int id) : base(m => m.Id == id)
+        {
+            AddInclude("Recipes.Ingredient.BranchStocks");
+        }
+    }
+}
