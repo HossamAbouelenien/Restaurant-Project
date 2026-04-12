67ed7ecbb4f6619383ed189a601d6e4ef8291fd5
mustafas3aad <mostafasaadtornado7@gmail.com>
2026-04-12 22:10:41 +0200

    Add Avaliable Drivers EndPoint

---
+++ RMS.Services/Specifications/DeliverySpec/AvailableDriversSpecification.cs
@@ -0,0 +1,21 @@
+﻿using RMS.Domain.Entities;
+using RMS.Domain.Enums;
+using RMS.Services.Specifications;
+
+public class AvailableDriversSpecification : BaseSpecifications<User>
+{
+    public AvailableDriversSpecification(AvailableDriversQueryParams query)
+        : base(u =>
+            u.RoleId == "Driver" &&
+            (query.BranchId == null || u.BranchId == query.BranchId) &&       
+            (string.IsNullOrEmpty(query.Search) ||
+             u.Name.Contains(query.Search) ||
+             u.PhoneNumber.Contains(query.Search)) &&
+            !u.Deliveries.Any(d => d.DeliveryStatus != DeliveryStatus.Delivered)
+        )
+    {
+       
+        AddInclude(u => u.Branch!);
+        ApplyPagination(query.PageSize, query.PageIndex);
+    }
+}
---
+++ RMS.Shared/QueryParams/AvailableDriversQueryParams.cs
@@ -0,0 +1,41 @@
+﻿public class AvailableDriversQueryParams
+{
+    #region Filtering
+
+    public int? BranchId { get; set; }
+
+    public string? Search { get; set; } 
+
+    #endregion
+
+  
+
+    #region Pagination
+
+    private int _pageIndex = 1;
+    public int PageIndex
+    {
+        get => _pageIndex;
+        set => _pageIndex = value <= 0 ? 1 : value;
+    }
+
+    private const int DefaultPageSize = 5;
+    private const int MaxPageSize = 10;
+    private int _pageSize = DefaultPageSize;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set
+        {
+            if (value <= 0)
+                _pageSize = DefaultPageSize;
+            else if (value > MaxPageSize)
+                _pageSize = MaxPageSize;
+            else
+                _pageSize = value;
+        }
+    }
+
+    #endregion
+}
