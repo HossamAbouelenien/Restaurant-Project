12d23ae88f43cb689152064184edde9dccce3871
mustafas3aad <mostafasaadtornado7@gmail.com>
2026-04-25 01:07:50 +0300

    Add ImageAsset entity to represent image metadata
    Created ImageAsset.cs in RMS.Domain.Entities with properties for Id, Url, PublicId, Width, Height, and Format to store image asset metadata.

---
+++ RMS.Domain/Entities/ImageAsset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Domain.Entities
+{
+    public class ImageAsset
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string PublicId { get; set; }
+
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public string? Format { get; set; }
+    }
+}
