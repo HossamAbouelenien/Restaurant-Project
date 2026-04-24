d6d8dc2fbcffd4eb2c98b2b9519cf9d0186797e0
mustafas3aad <mostafasaadtornado7@gmail.com>
2026-04-25 01:14:49 +0300

    Add Cloudinary image storage strategy and validation
    Implemented CloudinaryStorageStrategy for image upload/delete with validation and logging. Added ImageValidator for file type and size checks. Introduced IImageStorageStrategy and IImageValidator interfaces.

---
+++ RMS.ServicesAbstraction/IImageServices/IImageStorageStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using RMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.ServicesAbstraction.IImageServices
+{
+    public interface IImageStorageStrategy
+    {
+        Task<ImageAsset?> UploadAsync(IFormFile file);
+        Task<bool> DeleteAsync(string publicId);
+    }
+}
