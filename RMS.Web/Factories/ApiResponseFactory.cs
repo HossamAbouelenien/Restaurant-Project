0f63c8808592c622b286dbe69f20c55dbc870946
hossam abouelenien <hossamelbaz172@gmail.com>
2026-04-27 06:31:05 +0300

    Add ApiResponseFactory for standardized validation responses
    Introduce a new static class `ApiResponseFactory` in the
    `RMS.Web.Factories` namespace with a method
    `GenerateApiValidationResponse` to handle validation errors
    using `ProblemDetails`. Configure this method in the ASP.NET
    Core pipeline by updating `options.InvalidModelStateResponseFactory`
    in `Program.cs`. Minor adjustments made to the project file.

---
+++ RMS.Web/Factories/ApiResponseFactory.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.Web.Factories
+{
+    public static class ApiResponseFactory
+    {
+
+
+        public static IActionResult GenerateApiValidationResponse(ActionContext actionContext)
+        {
+            var Errors = actionContext.ModelState.Where(x => x.Value.Errors.Count > 0)
+                   .ToDictionary(x => x.Key, x => x.Value.Errors.Select(x => x.ErrorMessage).ToArray());
+
+            var problem = new ProblemDetails()
+            {
+
+
+                Title = "Validation Errors",
+
+                Detail = "One or more validation error occurred",
+
+                Status = StatusCodes.Status400BadRequest,
+
+                Extensions = { { "Errors", Errors } }
+
+
+
+            };
+
+
+            return new BadRequestObjectResult(problem);
+
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
